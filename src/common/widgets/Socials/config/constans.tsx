2caa882018a1cc522dbce88ddc92de5d8522866c
bar_ozery <barozer8@gmail.com>
2024-03-07 18:15:00 +0200

    added social

--- src/common/widgets/Socials/config/constans.tsx
+++ src/common/widgets/Socials/config/constans.tsx
@@ -1,9 +1,12 @@
-import { FiGithub, FiLinkedin } from 'react-icons/fi'
+import { FiGithub, FiLinkedin, FiTwitter, FiInstagram, FiFacebook } from 'react-icons/fi'
 import { SocialTypes } from './types'
 
 
 // social media data
 export const socialData : SocialTypes[] = [
     { icon : FiGithub, url : "https://github.com/barozer11" },
-    { icon : FiLinkedin, url : "https://www.linkedin.com/in/bar-ozery-b96a6921b/" }
+    { icon : FiLinkedin, url : "https://www.linkedin.com/in/bar-ozery-b96a6921b/" },
+    { icon : FiTwitter, url : "" },
+    { icon : FiInstagram, url : "" },
+    { icon : FiFacebook, url : "" },
 ]
