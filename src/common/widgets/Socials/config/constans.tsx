9d7400109b89d6bc9b15a7998a6e48840cf36e35
bar_ozery <barozer8@gmail.com>
2024-03-07 18:25:47 +0200

    rm ss

--- src/common/widgets/Socials/config/constans.tsx
+++ src/common/widgets/Socials/config/constans.tsx
@@ -6,4 +6,5 @@ import { SocialTypes } from './types'
 export const socialData : SocialTypes[] = [
     { icon : FiGithub, url : "https://github.com/barozer11" },
     { icon : FiLinkedin, url : "https://www.linkedin.com/in/bar-ozery-b96a6921b/" },
+    { icon : FiFacebook, url : "" },
 ]
